97cd3d0b16c5785d0f7435233331f656b3cbecea
ksw <dhjkl123@naver.com>
2025-05-12 23:43:33 +0900

    init project

---
+++ myboardserver/src/main/java/com/myboardserver/myboardserver/MyboardserverApplication.java
@@ -0,0 +1,13 @@
+package com.myboardserver.myboardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyboardserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyboardserverApplication.class, args);
+	}
+
+}
---
+++ myboardserver/src/test/java/com/myboardserver/myboardserver/MyboardserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myboardserver.myboardserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyboardserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
